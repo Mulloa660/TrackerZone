f6ffa6ffcdf6212214dedc7eef5be0e63bc6d250
Michelle Ulloa <umichelle244@gmail.com>
2023-05-01 14:51:59 -0400

    Transaction class was added. Working on the menu and switch cases.

--- src/main/java/com/learntocode/TrackerZone.java
+++ src/main/java/com/learntocode/TrackerZone.java
@@ -1,7 +1,59 @@
 package com.learntocode;
 
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TrackerZone {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Transaction transaction = new Transaction();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("transactions.csv")) {
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String choice = "";
+//The while loop continues until the user enters x to exit the app.
+        while (!choice.equalsIgnoreCase("x")) {
+//Prompt user to choose an option from the menu.
+            System.out.println("Welcome to Tracker Zone!");
+            System.out.println("===========================");
+            System.out.println("D - Add Deposit");
+            System.out.println("P - Make Payment");
+            System.out.println("L - Ledger");
+            System.out.println("X - Exit");
+            System.out.println("Select an option (D-X):");
+
+            choice = scanner.nextLine();
+
+            switch (choice) {
+                case "D":
+
+                    break;
+                case "P":
+
+                    break;
+                case "L":
+
+                    break;
+                case "X":
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+
+        }
     }
 }
+
---
+++ src/main/java/com/learntocode/Transaction.java
@@ -0,0 +1,57 @@
+package com.learntocode;
+
+public class Transaction {
+    private int date;
+    private int time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction() {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
