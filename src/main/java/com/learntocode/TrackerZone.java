205da9ecd2cfa6ff108343019ca8f043c26fcfa5
Michelle Ulloa <umichelle244@gmail.com>
2023-05-02 13:07:51 -0400

    Error with buffered reader was fixed

--- src/main/java/com/learntocode/TrackerZone.java
+++ src/main/java/com/learntocode/TrackerZone.java
@@ -2,27 +2,39 @@ package com.learntocode;
 
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class TrackerZone {
+    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         try {
             BufferedReader br = new BufferedReader(new FileReader("transactions.csv"));
+            String line;
             while ((line = br.readLine()) != null) {
-
-
                 String[] parts = line.split("\\|");
-                int id = Integer.parseInt(parts[0]);
-                String name = parts[1];
-                double price = Double.parseDouble(parts[2]);
-                inventory.add(new
-
-                        transactions(id, name, price));
+                String date = parts[0];
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate fecha = LocalDate.parse(date, formatter);
+                String time = parts[1];
+                DateTimeFormatter form = DateTimeFormatter.ofPattern("HH:mm:ss");
+                LocalTime tiempo = LocalTime.parse(time, form);
+                String description = parts[2];
+                String vendor = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+                transactions.add(new Transaction(fecha, tiempo, description, vendor, amount));
+                //arraylist.add(new 'constructor class'(LocalDate, LocalTime, String, String, double))
             }
         } catch (Exception e) {
             System.out.println("An error has occurred!");
@@ -45,23 +57,35 @@ public class TrackerZone {
 
             switch (choice) {
                 case "D":
-
-                    break;
-                case "P":
-
-                    break;
-                case "L":
-
-                    break;
-                case "X":
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                    private static void addDeposit
+                    System.out.println("Enter the deposit information:");
+                    System.out.println("Date (yyyy-mm-dd:");
+                    String date = scanner.nextLine();
+                    System.out.println("Description:");
+                    String description = scanner.nextLine();
+                    System.out.println("Vendor:");
+                    String vendor = scanner.nextLine();
+                    System.out.println("Amount:");
+                    double amount = scanner.nextDouble();
+                    scanner.nextLine();
             }
+            break;
+            case "P":
 
+                break;
+            case "L":
 
+                break;
+            case "X":
+                System.out.println("Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
         }
     }
+
+        scanner.close();
 }
+}
+
 
